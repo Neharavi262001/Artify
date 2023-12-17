66f1e4625f15e789da343c8cf09ac6ac1f5aef6c
Neharavi262001 <neharavi262001@gmail.com>
2023-12-17 13:00:33 +0530

    updated popup

--- artify/src/js/index.js
+++ artify/src/js/index.js
@@ -211,11 +211,18 @@ function displayImageDetails(imageDetails) {
   const modal = document.getElementById('modalContent');
   const closeModalBtn = document.getElementById('closeModalBtn');
   const modalContent = `
-  <img src="${imageDetails.imageUrl}" alt="${imageDetails.title}">
-  <h3>${imageDetails.title}</h3>
-  <p>${imageDetails.description}</p>
+  <a href="${imageDetails.imageUrl}" target="_blank">
+  <img src="${imageDetails.imageUrl}" alt="${imageDetails.title}" title="Click to see full image">
+</a>
+  <h3>${imageDetails.description.toUpperCase()}</h3>
   
-`;
+  <p>Author : ${imageDetails.author.first_name}</p>
+  <p>Updated at : ${imageDetails.author.updated_at}</p>
+  <a href='${imageDetails.author.links.html}' target="_blank">See more</a>
+  
+`
+;
+console.log(imageDetails)
   modal.innerHTML = modalContent;
   document.getElementById('overlay').style.display = 'block';
   document.getElementById('modal').style.display = 'block';
